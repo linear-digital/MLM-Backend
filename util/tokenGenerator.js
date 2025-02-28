2e2f6d30355bfa0994eacf7e4bc5541ac4e11409
linear-digital <tamizuddin.tr@gmail.com>
2025-02-28 12:25:22 +0600

    update tokenGenerator.js

--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const secret = process.env.JWT_SECRET || "default_secret"; // Ensure a fallback secret
+const secret = "Veryverysecret1574349"; // Ensure a fallback secret
 
 const tokenGenerator = (user) => {
     try {
