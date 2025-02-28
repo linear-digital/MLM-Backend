83b48ce228046e9ac1a1d757db5a637463f146af
linear-digital <tamizuddin.tr@gmail.com>
2025-02-28 17:02:51 +0600

    update tokenGenerator.js

--- util/tokenGenerator.js
+++ util/tokenGenerator.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 
-const secret = process.env.JWT_SECRET
+const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcyMDg4OTQ0MiwiaWF0IjoxNzIwODg5NDQyfQ.dArWtfCxb9x1SKeTu5-_FML7V4SDYVDa6lZLzFXRCcg";
 const tokenGenerator = (user) => {
     const token = jwt.sign({ id: user._id, role: user.role }, secret, { expiresIn: "30d" });
     return token;
