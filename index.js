a5f3baaa7bdec039024bef990e733a37bd86dc39
linear-digital <tamizuddin.tr@gmail.com>
2025-03-03 14:31:33 +0600

    update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
 
 app.use(cors(corsOptions));
